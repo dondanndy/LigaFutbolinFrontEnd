436b47e68f2fe304c4a4917144006fe410dd93d6
Daniel Durán <daniel.10.10.1997@gmail.com>
2022-11-27 20:47:49 +0100

    Add a little fadeIn effect to loadable content

--- src/components/loadableContent.svelte
+++ src/components/loadableContent.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import { fade } from 'svelte/transition';
+  import { quartInOut } from 'svelte/easing';
+
 	import { Spinner } from "flowbite-svelte";
   import { DataLoadingStatus } from "../models/models";
 
@@ -7,9 +10,15 @@
 
 
 {#if dataLoadingStatus == DataLoadingStatus.Loading}
-  <div class="flex flex-col justify-center items-center h-48">
+  <div class="flex flex-col justify-center items-center h-48"
+    out:fade="{{ duration: 150, easing:quartInOut }}" 
+  >
     <Spinner color="white"></Spinner>
   </div>
 {:else}
-  <slot/>
+  <div
+    in:fade="{{ duration: 250, delay: 150, easing:quartInOut }}"
+    >
+    <slot/>
+  </div>
 {/if}
